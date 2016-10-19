044beee5066d9a8426f5a59d231dff230d20b945
Amanda Schultz <amanda.cecilia.schultz@gmail.com>
2016-10-19 14:04:17 +0200

    Create heat_2.js

---
+++ heat_2.js
@@ -0,0 +1,162 @@
+/*EXTREME JS*/
+
+$(document).ready(function(){
+	$("#pic1").click(function(){
+		$(".desc1").slideToggle("slow");
+	});
+});
+
+$(document).ready(function(){
+	$("#pic2").click(function(){
+		$(".desc2").slideToggle("slow");
+	});
+});
+
+$(document).ready(function(){
+	$("#pic3").click(function(){
+		$(".desc3").slideToggle("slow");
+	});
+});
+$(document).ready(function(){
+	$("#pic4").click(function(){
+		$(".desc4").slideToggle("slow");
+	});
+});
+$(document).ready(function(){
+	$("#pic5").click(function(){
+		$(".desc5").slideToggle("slow");
+	});
+});
+$(document).ready(function(){
+	$("#pic6").click(function(){
+		$(".desc6").slideToggle("slow");
+	});
+});
+
+$(document).ready(function(){
+	$("#pic7").click(function(){
+		$(".desc7").slideToggle("slow");
+	});
+});
+
+$(document).ready(function(){
+	$("#pic8").click(function(){
+		$(".desc8").slideToggle("slow");
+	});
+});
+$(document).ready(function(){
+	$("#pic9").click(function(){
+		$(".desc9").slideToggle("slow");
+	});
+});
+
+/*COMEDY JS*/
+
+$(document).ready(function(){
+	$("#cpic1").click(function(){
+		$(".desc1").slideToggle("slow");
+	});
+});	
+$(document).ready(function(){
+	$("#cpic2").click(function(){
+		$(".desc2").slideToggle("slow");
+	});
+});
+$(document).ready(function(){
+	$("#cpic3").click(function(){
+		$(".desc3").slideToggle("slow");
+	});
+});
+$(document).ready(function(){
+	$("#cpic4").click(function(){
+		$(".desc4").slideToggle("slow");
+	});
+});
+$(document).ready(function(){
+	$("#cpic5").click(function(){
+		$(".desc5").slideToggle("slow");
+	});
+});
+$(document).ready(function(){
+	$("#cpic6").click(function(){
+		$(".desc6").slideToggle("slow");
+	});
+});	
+$(document).ready(function(){
+	$("#cpic7").click(function(){
+		$(".desc7").slideToggle("slow");
+	});
+});
+$(document).ready(function(){
+	$("#cpic8").click(function(){
+		$(".desc8").slideToggle("slow");
+	});
+});
+/*ARTZY JS*/
+
+$(document).ready(function(){
+	$("#apic1").click(function(){
+		$(".desc1").slideToggle("slow");
+	});
+});	
+$(document).ready(function(){
+	$("#apic2").click(function(){
+		$(".desc2").slideToggle("slow");
+	});
+});
+$(document).ready(function(){
+	$("#apic3").click(function(){
+		$(".desc3").slideToggle("slow");
+	});
+});
+$(document).ready(function(){
+	$("#apic4").click(function(){
+		$(".desc4").slideToggle("slow");
+	});
+});
+$(document).ready(function(){
+	$("#apic5").click(function(){
+		$(".desc5").slideToggle("slow");
+	});
+});
+$(document).ready(function(){
+	$("#apic6").click(function(){
+		$(".desc6").slideToggle("slow");
+	});
+});
+
+/*NO_BOX JS*/
+
+$(document).ready(function(){
+	$("#npic1").click(function(){
+		$(".desc1").slideToggle("slow");
+	});
+});	
+$(document).ready(function(){
+	$("#npic2").click(function(){
+		$(".desc2").slideToggle("slow");
+	});
+});
+$(document).ready(function(){
+	$("#npic3").click(function(){
+		$(".desc3").slideToggle("slow");
+	});
+});
+$(document).ready(function(){
+	$("#npic4").click(function(){
+		$(".desc4").slideToggle("slow");
+	});
+});
+$(document).ready(function(){
+	$("#npic5").click(function(){
+		$(".desc5").slideToggle("slow");
+	});
+});
+
+/* HEAT2 JS*/
+
+$(document).ready(function(){
+	$("#arrow").click(function(){
+		$(".box2").slideToggle("slow");
+	});
+});
